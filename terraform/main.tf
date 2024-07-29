a7aa3c119672471ec1dc21b051f2207cbdfd5262
alonit <alon.itach@mail.huji.ac.il>
2024-07-29 19:57:50 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,15 @@
+terraform {
+    backend "s3" {
+    bucket = "alonit-tf-state-files"
+    key    = "tfstate.json"
+    region = "us-east-2"
+    # optional: dynamodb_table = "<table-name>"
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
+
 }
 
 resource "aws_instance" "example" {
